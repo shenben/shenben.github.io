ae9e16ebda1e8080b87fd154b1f08c84f32b93c0
shenben <1728816406@qq.com>
2024-08-14 16:31:57 +0800

    phonetic symbol for the name

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 # git reset --hard 1d046b754b05316be5900e2bff06e514785c102c
 # git push origin HEAD --force
 git add .
-git commit -m "without png displaying" 
+git commit -m "phonetic symbol for the name" 
 git push
